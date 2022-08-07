280972b56857b8ac7df589d923dd04ecae97098d
phattaranun amornnun <phattaranun.a@wirtual.co>
2022-08-07 16:52:00 +0700

    dydamic username password mqtt broker

--- libs/mymqtt/mymqtt.go
+++ libs/mymqtt/mymqtt.go
@@ -21,8 +21,12 @@ func ConnectMQTT() {
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(fmt.Sprintf("tls://%s:%s", myenv.MQTT_BROKER, myenv.MQTT_PORT))
 	opts.SetClientID("golang-script")
-	// opts.SetUsername(myenv.MQTT_USERNAME) // these are the credentials that you declare for your cluster
-	// opts.SetPassword(myenv.MQTT_PASSWORD)
+	if myenv.MQTT_USERNAME != "" {
+		opts.SetUsername(myenv.MQTT_USERNAME)
+	}
+	if myenv.MQTT_USERNAME != "" {
+		opts.SetPassword(myenv.MQTT_PASSWORD)
+	}
 
 	opts.OnConnect = connectHandler
 	opts.OnConnectionLost = connectLostHandler
