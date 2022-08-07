046fcb68b0037e31925804928742b7b72e30e379
phattaranun amornnun <phattaranun.a@wirtual.co>
2022-08-07 16:50:44 +0700

    change free mqtt broker

--- libs/mymqtt/mymqtt.go
+++ libs/mymqtt/mymqtt.go
@@ -21,8 +21,8 @@ func ConnectMQTT() {
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(fmt.Sprintf("tls://%s:%s", myenv.MQTT_BROKER, myenv.MQTT_PORT))
 	opts.SetClientID("golang-script")
-	opts.SetUsername(myenv.MQTT_USERNAME) // these are the credentials that you declare for your cluster
-	opts.SetPassword(myenv.MQTT_PASSWORD)
+	// opts.SetUsername(myenv.MQTT_USERNAME) // these are the credentials that you declare for your cluster
+	// opts.SetPassword(myenv.MQTT_PASSWORD)
 
 	opts.OnConnect = connectHandler
 	opts.OnConnectionLost = connectLostHandler
